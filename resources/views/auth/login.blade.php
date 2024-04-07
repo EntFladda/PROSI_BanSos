1f04dce7732e447db8f34ca83f964e97945c5226
EntFla <adadgamer01@gmail.com>
2024-04-07 19:14:24 +0700

    login page 1.3

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -18,6 +18,7 @@
             }
             .button{
                 display: inline-block;
+                width: 110%;
                 font-weight: 400;
                 color: #ffffff;
                 text-align: center;
@@ -36,16 +37,16 @@
             }
             .form{
                 display: block;
-                width: 100%;
+                width: 110%;
                 height: calc(1.5em + .75rem + 2px);
                 padding: 1.5rem 15rem;
                 font-size: 1rem;
                 font-weight: 400;
                 line-height: 1.5;
-                color: #495057;
-                background-color: #fff;
+                color: #000000;
+                background-color: #ffffff;
                 background-clip: padding-box;
-                border: 1px solid #000000;
+                border: 1px solid #101C2C;
                 border-radius: .75rem;
                 transition: border-color .15s ease-in-out, box-shadow .15s ease-in-out;
             }
