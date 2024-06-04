8a685ee6ffe4d5706cff553b24b3439ada1c08a4
aalvia123 <115689780+aalvia123@users.noreply.github.com>
2024-06-05 00:36:25 +0700

    seeder data dont fix

--- database/seeders/DataTableSeeder.php
+++ database/seeders/DataTableSeeder.php
@@ -14,76 +14,28 @@ class DataTableSeeder extends Seeder
      */
     public function run(): void
     {
-        $data = [
+        $data = [];
 
-            [
-                'id_login' => 1,
-                'nama' => 'Jennie',
-                'no_KK' => '1234567890123456',
-                'no_tlp' => '081234567890',
-                'penghasilan' => 5000000,
-                'Pekerjaan' => 'Software Engineer',
+        for ($i = 1; $i <= 50; $i++) {
+            $data[] = [
+                'id_login' => $i,
+                'nama' => 'User' . $i,
+                'no_KK' => str_repeat($i, 16),
+                'no_tlp' => '08' . str_repeat($i, 10),
+                'penghasilan' => 5000000 + ($i * 10000),
+                'Pekerjaan' => 'Pekerjaan' . $i,
                 'kondisi_rumah' => 'Layak huni',
-                'jumlah_tanggungan' => 3,
-                'status' => true,
+                'jumlah_tanggungan' => rand(1, 5),
+                'sktm' => 'sktm_' . $i . '.png',
+                'slip_gaji' => 'slip_gaji_' . $i . '.png',
+                'foto_depan' => 'foto_depan_' . $i . '.png',
+                'foto_ruang_tamu' => 'foto_ruang_tamu_' . $i . '.png',
+                'foto_dapur' => 'foto_dapur_' . $i . '.png',
+                'status' => rand(0, 1) == 1,
                 'created_at' => now(),
-                'updated_at' => now(),
-            ],
-            [
-                'id_login' => 2,
-                'nama' => 'Rose',
-                'no_KK' => '1234567890123456',
-                'no_tlp' => '081234567890',
-                'penghasilan' => 5000000,
-                'Pekerjaan' => 'Software Engineer',
-                'kondisi_rumah' => 'Layak huni',
-                'jumlah_tanggungan' => 3,
-                'status' => true,
-                'created_at' => now(),
-                'updated_at' => now(),
-            ],
-            [
-                'id_login' => 1,
-                'nama' => 'Jennie',
-                'no_KK' => '1234567890123456',
-                'no_tlp' => '081234567890',
-                'penghasilan' => 5000000,
-                'Pekerjaan' => 'Software Engineer',
-                'kondisi_rumah' => 'Layak huni',
-                'jumlah_tanggungan' => 3,
-                'status' => true,
-                'created_at' => now(),
-                'updated_at' => now(),
-            ],
-            [
-                'id_login' => 1,
-                'nama' => 'Jennie',
-                'no_KK' => '1234567890123456',
-                'no_tlp' => '081234567890',
-                'penghasilan' => 5000000,
-                'Pekerjaan' => 'Software Engineer',
-                'kondisi_rumah' => 'Layak huni',
-                'jumlah_tanggungan' => 3,
-                'status' => true,
-                'created_at' => now(),
-                'updated_at' => now(),
-            ],
-            [
-                'id_login' => 1,
-                'nama' => 'Jennie',
-                'no_KK' => '1234567890123456',
-                'no_tlp' => '081234567890',
-                'penghasilan' => 5000000,
-                'Pekerjaan' => 'Software Engineer',
-                'kondisi_rumah' => 'Layak huni',
-                'jumlah_tanggungan' => 3,
-                'status' => true,
-                'created_at' => now(),
-                'updated_at' => now(),
-            ],
-
-        ];
-
+                'updated_at' =>now(),
+            ];
+        }
         DB::table('data')->insert($data);
     }
 }
