70ff52d18f82a6802cb1cb575bda566baf3118b4
aalvia123 <115689780+aalvia123@users.noreply.github.com>
2024-06-18 09:06:58 +0700

    delete menu aras

--- resources/views/rw/data_kriteria_aras.blade.php
+++
@@ -1,3 +0,0 @@
-@extends('rw.template')
-
-
--- resources/views/rw/validasi.blade.php
+++ resources/views/rw/validasi.blade.php
@@ -99,7 +99,7 @@
                 <div class="info-button">
                     <input type="text" id="search-input" placeholder="Search...">
                 </div>
-                <h3 style="font-size:25px;font-weight:bold;text-align:center;">Menunggu Persetujuan</h3>
+                <h3 style="font-size:30px;font-weight:bold;text-align:center;">Menunggu Persetujuan</h3>
                 <table>
                     <tr>
                         <th style="text-align: center;">No</th>
